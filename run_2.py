6468a5e4791f5ac23da84a28d293e52acfc67a71
Max Malynowsky <github@maxmalynowsky.com>
2022-01-15 20:58:10 -0500

    add run code snippets

--- run_2.py
+++ run_2.py
@@ -5,3 +5,4 @@ funcs = ['extended', 'meta', 'edge_extender']
 if __name__ == '__main__':
     for func in funcs:
         subprocess.run(['python3', '-m', f'processing.{func}'])
+    subprocess.run(['python3', 'sync.py'])
